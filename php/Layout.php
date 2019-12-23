3695fe659adf99286e17cc4665141c8d054af38c
iakigarciaL <garcianoya@hotmail.es>
2019-12-23 15:47:55 +0100

    Diseño terminado

---
+++ php/Layout.php
@@ -0,0 +1,31 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <?php include '../html/Head.html' ?>
+</head>
+<body>
+    <?php include '../html/Header.html' ?>
+    <div class="container">
+        <div class="row">
+            <div class="col-md-3" >
+                <div class="card">
+
+                </div>
+            </div>
+
+            <div class="col-md-6" >
+                <div class="card">
+                    
+                </div>
+            </div>
+
+            <div class="col-md-3">
+                <div class="card">
+                    
+                </div>
+            </div>
+        </div>
+    </div>
+    <?php include '../html/Footer.html' ?>
+</body>
+</html>
---
+++ php/Navbar.php
@@ -0,0 +1,13 @@
+<nav class="navbar">
+    <ul class="navbar-nav">
+        <li class="nav-item">
+        <a href="" title=""><i style="margin-right: 0.5em; color: #EEEEEE;" class="icon-home icon-4x"></i>Website Link</a>
+        </li>
+        <li class="nav-item">
+            <a class="nav-link" href="#">Link 2</a>
+        </li>
+        <li class="nav-item">
+            <a class="nav-link" href="#">Link 3</a>
+        </li>
+    </ul>
+</nav>
